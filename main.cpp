c37c9df08087ec455a262031810aba55b6516645
ducminhyeungocthuy <nddminh2021@gmail.com>
2022-07-31 00:21:09 +0700

    fundamental functions working properly, TO BE ADDED SOON: fractionBeautififer()

--- main.cpp
+++ main.cpp
@@ -1,16 +1,61 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <memory>
-#include <cmath>
 #include <sstream>
+#include <utility>
 
 using namespace std;
+
+int gcd(int& a, int& b)
+{
+    if (a==0 || b==0) return 0;
+
+	int GCD, n1=abs(a), n2=abs(b);
+	if (n1>n2) //n2 must be greater than n1
+	{
+		int tmp = n1;
+		n1 = n2;
+		n2 = tmp;
+	}
+
+	for (int i = 1; i <= n1; i++)
+	{
+		if (n1%i==0 && n2%i==0) {GCD=i;}
+	}
+
+	return GCD;
+}
+
+void reduceFraction(pair<int, int>& p)
+{	
+	int GCD = gcd(p.first, p.second);
+	if (GCD==0 || GCD==1) {return;}
+	else
+	{
+		p.first = (p.first / GCD);
+		p.second = (p.second / GCD);
+	}
+}
+
+void readFraction(string& s, vector<pair<int, int>>& v)
+{
+	int FIRST, SECOND;
+	for (int i=0; i<s.length(); i++)
+	{
+		if (s[i]=='/')
+		{
+			FIRST = stoi(s.substr(0, i));
+			SECOND = stoi(s.substr(i+1, s.length()-i));
+			if (SECOND==0) {cout << "DIVIDE BY 0!"; exit(0);}
+			v.push_back(make_pair(FIRST, SECOND));
+		}
+	}
+}
 
 int main()
 {
 	ifstream inputfile("input.txt");
-	ofstream outputfile("output.txt", ios::trunc);
+	ofstream outputfile("output.txt");
 	if (inputfile.good() && outputfile.good())
 	{
 		cout << "Accessed file successfully" << endl;
@@ -26,19 +71,30 @@ int main()
 		count++;
 	}
 
-	vector<float> mat;
-	mat.reserve(rows*cols);
+	int size=rows*cols;
+	vector<pair<int, int>> mat;
+	mat.reserve(size);
 	// read matrix
 	while (getline(inputfile, line))
 	{
 		istringstream ss(line);
-		double tmp;
-		while (ss>>tmp)
+		string fractionString;
+		while (ss >> fractionString)
 		{
-			mat.push_back(tmp);
+			readFraction(fractionString, mat);
 		}
 	}
-	
-	for (auto num : mat) {cout << num << ' ';}
+
+	for (int i=0; i<size; i++)
+	{
+		reduceFraction(mat[i]);
+	}
+
+	//write result to outputfile
+	for (int i=0; i<size; i++)
+	{
+		outputfile << mat[i].first << '/' << mat[i].second << ' ';
+		if ((i+1)%cols==0) {outputfile << endl;}
+	}
 	return 0;
 }
